a19d47763e930de4d9b338dc253159e2771c8d2e
Tom Thorne <tom.thorne@pepperhq.com>
2022-12-29 16:36:44 +0000

    expose location-overrides for modifiers

--- src/models/catalogModifier.ts
+++ src/models/catalogModifier.ts
@@ -1,4 +1,5 @@
 import {
+  array,
   lazy,
   nullable,
   number,
@@ -8,6 +9,30 @@ import {
   string,
 } from '../schema';
 import { Money, moneySchema } from './money';
+
+
+export interface ModifierLocationOverrides {
+  /** The ID of the `Location`. This can include locations that are deactivated. */
+  locationId?: string | null;
+  /**
+   * Represents an amount of money. `Money` fields can be signed or unsigned.
+   * Fields that do not explicitly define whether they are signed or unsigned are
+   * considered unsigned and can only hold positive amounts. For signed fields, the
+   * sign of the value indicates the purpose of the money transfer. See
+   * [Working with Monetary Amounts](https://developer.squareup.com/docs/build-basics/working-with-monetary-amounts)
+   * for more information.
+   */
+  priceMoney?: Money;
+}
+
+export const modifierLocationOverridesSchema: Schema<ModifierLocationOverrides> = object(
+  {
+    locationId: ['location_id', optional(nullable(string()))],
+    priceMoney: ['price_money', optional(lazy(() => moneySchema))],
+    pricingType: ['pricing_type', optional(string())]
+  }
+);
+
 
 /** A modifier applicable to items at the time of sale. */
 export interface CatalogModifier {
@@ -31,6 +56,7 @@ export interface CatalogModifier {
    * Currently this image is not displayed by Square, but is free to be displayed in 3rd party applications.
    */
   imageId?: string | null;
+  locationOverrides?: ModifierLocationOverrides[] | null;
 }
 
 export const catalogModifierSchema: Schema<CatalogModifier> = object({
@@ -39,4 +65,8 @@ export const catalogModifierSchema: Schema<CatalogModifier> = object({
   ordinal: ['ordinal', optional(nullable(number()))],
   modifierListId: ['modifier_list_id', optional(nullable(string()))],
   imageId: ['image_id', optional(nullable(string()))],
+  locationOverrides: [
+    'location_overrides',
+    optional(nullable(array(lazy(() => modifierLocationOverridesSchema)))),
+  ],  
 });
